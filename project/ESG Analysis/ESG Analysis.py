e27e4d461d6611e7f5ec629b675290ec0ea41aa9
Jay_desktop_home <bazli8520g79@gmail.com>
2024-04-17 22:01:32 +0800

    ADD pyspark.ml.regression

--- project/ESG Analysis/ESG Analysis.py
+++ project/ESG Analysis/ESG Analysis.py
@@ -52,7 +52,7 @@ import matplotlib.pyplot as plt
 pandas_df = df.toPandas()
 # plot
 for indicator_name in indicator_names:
-    # 設置新的圖形
+    # set plot style
     plt.figure(figsize=(6, 6))
     plt.title(indicator_name)
     plt.xlabel('Year')
@@ -60,26 +60,22 @@ for indicator_name in indicator_names:
     plt.grid(True)
 
     for index, row in pandas_df.iterrows():
-        # 提取國家名稱和指標數據
         country = row['Country Name']
         values = [row[str(year)] for year in range(1984, 2021)]
-        # print
         if row['Indicator Name'] == indicator_name:
             plt.plot(range(1984, 2021), values, label=country)
     
-    # 添加圖例
+    # add legend
     plt.legend()
     plt.show()
 
-#### TEST ####
+### Prediction for CO2 emission ###
 
-# 過濾出 Indicator Name 為 CO2 emissions 的記錄
 co2_emissions_df = df.filter(col("Indicator Name") == "CO2 emissions (metric tons per capita)")
 co2_emissions_df.show(3)
-# 定義要保留的列，除了年份之外的其他列
 id_cols = ['Country Name', 'Indicator Name']
 
-# 將每個年份的CO2排放量轉換為單獨的行
+# Transpose table with 'stack' funtion
 co2_emissions_df_Expr = "stack(38, '1984', `1984`, '1985', `1985`, '1986', `1986`, '1987', `1987`, '1988', `1988`, '1989', `1989`, '1990', `1990`, '1991', `1991`, '1992', `1992`,\
                                    '1993', `1993`, '1994', `1994`, '1995', `1995`, '1996', `1996`, '1997', `1997`, '1998', `1998`, '1999', `1999`, '2000', `2000`, '2001', `2001`,\
                                    '2002', `2002`, '2003', `2003`, '2004', `2004`, '2005', `2005`, '2006', `2006`, '2007', `2007`, '2008', `2008`, '2009', `2009`, '2010', `2010`,\
@@ -95,34 +91,32 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql.functions import lit
 
 # Prepare features
-assembler = VectorAssembler(inputCols= ['CO2_emissions'],
-                            outputCol="features")
+co2_emissions_df = co2_emissions_df.withColumn("Year", co2_emissions_df["Year"].cast("int"))
+# update feature，including Year
+assembler = VectorAssembler(inputCols=['Year','CO2_emissions'], outputCol="features")
 data = assembler.transform(co2_emissions_df)
 data.printSchema()
 
-data = data.withColumn("Year", data["Year"].cast("double"))
-# 更新特徵向量內容，包含 Year
-assembler = VectorAssembler(inputCols=['Year', 'CO2_emissions'], outputCol="features")
-data = assembler.transform(data)
+# Rename CO2_emissions column to label
+data = data.withColumnRenamed("CO2_emissions", "label")
 
-
-# 預測 CO2 emissions
+lr = LinearRegression(featuresCol="features",
+                      labelCol="label",
+                      predictionCol="predicted_CO2_emissions")
+lr_model = lr.fit(data)
 predictions = lr_model.transform(data)
 predictions.show()
+
 # 創建一個空的 DataFrame 來存儲預測結果
 prediction_df = spark.createDataFrame([], co2_emissions_df.schema)
-# 遍歷 2021 年至 2030 年的每一年
+# Iterate over the years 2021 to 2030
 for year in range(2021, 2031):
-    # 準備測試數據
+    # Prepare test data
     test_data = data.withColumn("Year", lit(year))
     
-    # 訓練模型
-    lr = LinearRegression(featuresCol="features", labelCol="CO2_emissions", predictionCol=f"predicted_CO2_{year}")
-    lr_model = lr.fit(data)
-    
-    # 預測並添加到結果 DataFrame 中
+    # Predict and add to the result DataFrame
     predictions = lr_model.transform(test_data)
-    prediction_df = prediction_df.union(predictions.select("Country Name", "Year", f"predicted_CO2_{year}"))
+    prediction_df = prediction_df.union(predictions.select("Country Name", "Year", "predicted_CO2_emissions"))
 
 
 # stop SparkSession
