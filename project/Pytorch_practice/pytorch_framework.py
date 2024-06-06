bb12da5b3812c793c24d6c0525b950844bb3a3dc
Jay_desktop_home <bazli8520g79@gmail.com>
2024-06-06 23:22:29 +0800

    Create_practice_data

--- project/Pytorch_practice/pytorch_framework.py
+++ project/Pytorch_practice/pytorch_framework.py
@@ -1,5 +1,18 @@
 import torch
 from torch import nn
 import matplotlib.pyplot as plt 
-
 print('Torch Version :',torch.__version__)
+
+# Create known parameters
+weight = 0.7
+bias = 0.3
+# Create data
+start = 0
+end = 1
+step = 0.02
+# Create features
+X = torch.arange(start, end, step).unsqueeze(dim = 1)
+# Create labels
+Y = weight * X + bias
+X
+Y
